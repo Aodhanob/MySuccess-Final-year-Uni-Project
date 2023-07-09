65e3b7e45bfc08adb849e4f28ee12dcde8cb6657
Aodhanob <aodhanobrien7@gmail.com>
2023-07-09 18:09:16 +0100

    Changing to month view

--- src/app/home.component.ts
+++ src/app/home.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { AuthService } from '@auth0/auth0-angular';
+import { View } from '@syncfusion/ej2-angular-schedule';
 
 @Component({
   selector: 'home',
-  template: '<ejs-schedule></ejs-schedule>',
-  // templateUrl: './home.component.html',
+  templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent { 
+
+public setView: View = 'Month';
 
   constructor(public authService: AuthService){ }
 
