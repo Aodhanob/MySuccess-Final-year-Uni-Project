1e727385a5193ef4aa35f55314dbaa5f0b3c08e3
Aodhanob <aodhanobrien7@gmail.com>
2023-07-25 16:07:52 +0100

    Newest Changes

--- src/app/habits/habits.component.ts
+++ src/app/habits/habits.component.ts
@@ -2,42 +2,43 @@ import { Component } from '@angular/core';
 import { WebService } from '../web.service';
 import { ActivatedRoute } from '@angular/router';
 import { FormBuilder, Validators } from '@angular/forms';
+import { AuthService } from '@auth0/auth0-angular';
 
 @Component({
   selector: 'habits',
   templateUrl: './habits.component.html',
-  styleUrls: ['./habits.component.css']
+  styleUrls: ['./habits.component.css'],
 })
 export class HabitsComponent {
-
   habit_list: any = []; // stores habits property
   page: number = 1; // page number property
-  notes: any = [];  // stores notes property
+  notes: any = []; // stores notes property
 
   constructor(
-    public webService: WebService, 
-    private route: ActivatedRoute, 
-    private formBuilder: FormBuilder) { }
+    public webService: WebService,
+    private route: ActivatedRoute,
+    private formBuilder: FormBuilder,
+    public authService: AuthService
+  ) {}
 
   ngOnInit() {
-    if (sessionStorage['page']){
-      this.page = Number(sessionStorage['page'])
+    if (sessionStorage['page']) {
+      this.page = Number(sessionStorage['page']);
     }
-    
-    this.habit_list = this.webService.getHabits(this.page);   // get list of habits
-    this.notes = this.webService.getNotes(this.route.snapshot.params['id'] );   // get list of notes
 
+    this.habit_list = this.webService.getHabits(this.page); // get list of habits
+    this.notes = this.webService.getNotes(this.route.snapshot.params['id']); // get list of notes
   }
 
-  previousPage(){ 
-    if (this.page > 1){
+  previousPage() {
+    if (this.page > 1) {
       this.page = this.page - 1;
       sessionStorage['page'] = this.page;
       this.habit_list = this.webService.getHabits(this.page);
     }
   }
 
-  nextPage(){ 
+  nextPage() {
     this.page = this.page + 1;
     sessionStorage['page'] = this.page;
     this.habit_list = this.webService.getHabits(this.page);
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,19 +1,28 @@
 import { Component } from '@angular/core';
 import { AuthService } from '@auth0/auth0-angular';
-import { View } from '@syncfusion/ej2-angular-schedule';
+import { extend } from '@syncfusion/ej2-base';
+import {
+  EventSettingsModel,
+  View,
+  EventRenderedArgs,
+  DayService,
+  WeekService,
+  WorkWeekService,
+  MonthService,
+  AgendaService,
+  ResizeService,
+  DragAndDropService,
+} from '@syncfusion/ej2-angular-schedule';
 
 @Component({
   selector: 'home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
-  
+  styleUrls: ['./home.component.css'],
 })
-export class HomeComponent { 
-
-  styles?: string[]
+export class HomeComponent {
+  styles?: string[];
 
   public setView: View = 'Month'; // Calendar displayed as Month view for user.
 
-    constructor(public authService: AuthService){ }
-
+  constructor(public authService: AuthService) {}
 }
